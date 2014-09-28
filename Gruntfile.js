f02c12406e841bbf20fe533150c9f5036088c6e4
Titouan BENOIT <nightbringer4ever@gmail.com>
2014-09-28 10:59:02 +0200

    grunt clean fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
                     dot: true,
                     src: [
                         '.tmp',
-                        '<%= yeoman.dist %>/*',
+                        '<%= yeoman.dist %>/**/*',
                         '!<%= yeoman.dist %>/.git*'
                     ]
                 }]
